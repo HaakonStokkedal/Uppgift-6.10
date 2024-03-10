e0f3d1b133f8b6d46375258b1066884c965d1f64
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2024-03-10 19:32:57 +0100

    Add project files.

---
+++ Uppgift 6.10/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Uppgift_6._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en sträng:");
+            string text = Console.ReadLine();
+            string[] ord = text.Split(' ');
+
+            Console.WriteLine($"Det finns {AntalTalIText(ord)} tal i din string.");
+            Console.ReadKey();
+        }
+
+        static int AntalTalIText(string[] strings)
+        {
+            int räknare = 0;
+            foreach(string ord in strings)
+            {
+                if (double.TryParse(ord, out double value) == true)
+                {
+                    räknare++;
+                }
+            }
+            return räknare;
+        }
+    }
+}
